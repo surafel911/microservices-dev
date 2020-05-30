dddc5d3df8b9523d66c6ff6639318cc36f685919
Surafel Assefa <surafel911@gmail.com>
2020-05-29 23:23:22 -0400

    Adds database seeding and try/catch blocks.

---
+++ c#/PatientService/Models/SeedData.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Net.Http;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+using PatientService.Data;
+
+/*
+ * TODO: Use this API to seed users https://randomuser.me/api
+ */
+namespace PatientService.Models
+{
+    public static class SeedData
+    {
+		private static readonly int PatientCount = 5000;
+
+		private static async Task<string> GetRandomPatientData(HttpClient httpClient)
+		{
+            return await httpClient.GetStringAsync(
+				"https://randomuser.me/api/?nat=us&results=" + PatientCount.ToString());
+		}
+
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+			HttpClient httpClient;
+			PatientServiceDbContext patientServiceDbContext;
+
+			httpClient = serviceProvider.GetRequiredService<IHttpClientFactory>().CreateClient();
+            patientServiceDbContext = new PatientServiceDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<PatientServiceDbContext>>());
+            			
+            if (patientServiceDbContext.Patients.Any()) {
+                return;
+            }
+
+            ICollection<Patient> patients = new List<Patient>();
+            ICollection<PatientContact> patientContacts = new List<PatientContact>();
+            
+			JsonElement patientsElement = JsonDocument.Parse(GetRandomPatientData(httpClient).Result)
+				.RootElement.GetProperty("results");
+				
+			foreach (JsonElement patientElement in patientsElement.EnumerateArray()) {
+ 				JsonElement idElement = patientElement.GetProperty("id");
+				JsonElement nameElement = patientElement.GetProperty("name");
+				JsonElement emailAddressElement = patientElement.GetProperty("email");
+				JsonElement dateOfBirthElement = patientElement.GetProperty("dob");
+				JsonElement phoneNumberElement = patientElement.GetProperty("cell");
+	
+				Patient patient = new Patient 
+					{
+						Id = Guid.NewGuid(),
+						FirstName = nameElement.GetProperty("first").GetString(),
+						LastName = nameElement.GetProperty("last").GetString(),
+						LastFourOfSSN = idElement.GetProperty("value").GetString().Substring(7),
+						DateOfBirth = dateOfBirthElement.GetProperty("date").GetDateTime()
+					};
+	
+				patients.Add(patient);
+	
+				PatientContact patientContact = new PatientContact
+					{
+						PatientId = patient.Id,
+						Patient = patient,
+						PhoneNumber = phoneNumberElement.GetString(),
+						EmailAddress = emailAddressElement.GetString()
+					};
+			
+				patientContacts.Add(patientContact);
+			}
+
+			patientServiceDbContext.Patients.AddRange(patients);
+			patientServiceDbContext.PatientContacts.AddRange(patientContacts);
+
+            patientServiceDbContext.SaveChanges();
+        }
+    }
+}
--- c#/PatientService/Startup.cs
+++ c#/PatientService/Startup.cs
@@ -1,64 +1,114 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Collections.Generic;
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 using Npgsql;
 using Microsoft.EntityFrameworkCore;
 
 using PatientService.Data;
+using PatientService.Models;
 
 namespace PatientService
 {
-    public class Startup
-    {
-        public IConfiguration Configuration { get; }
+	public class Startup
+	{
+		public IConfiguration Configuration { get; }
 
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddHttpClient();
+			
+			services.AddControllers();
 
-            services.AddDbContext<PatientServiceDbContext>(options =>
-                options.UseNpgsql(Configuration.GetConnectionString("PatientServiceDbContext")));
-        }
+			services.AddDbContext<PatientServiceDbContext>(options =>
+				options.UseNpgsql(Configuration.GetConnectionString("PatientServiceDbContext")));
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            using (PatientServiceDbContext PatientServiceDbContext = app.ApplicationServices.CreateScope()
-                .ServiceProvider.GetRequiredService<PatientServiceDbContext>())
-            {
-                if (env.IsDevelopment())
-                {
-                    app.UseDeveloperExceptionPage();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			using (IServiceScope serviceScope = app.ApplicationServices.CreateScope()) {
+				ILogger<Startup> logger;
+				IServiceProvider serviceProvider;
+				PatientServiceDbContext patientServiceDbContext;
+				
+				serviceProvider = serviceScope.ServiceProvider;
+				logger = serviceProvider.GetRequiredService<ILogger<Startup>>();
+				patientServiceDbContext = serviceProvider.GetRequiredService<PatientServiceDbContext>();
 
-                    PatientServiceDbContext.Database.EnsureDeletedAsync();
-                    PatientServiceDbContext.Database.EnsureCreatedAsync();
-                }
-            }
+				try {
+					patientServiceDbContext.Database.CanConnect();
+				} catch (NpgsqlException e) {
+					logger.LogError(e, "An error occured when testing database connection.");
+					throw e;
+				}
 
-            app.UseRouting();
+				if (env.IsDevelopment()) {
+					app.UseDeveloperExceptionPage();
 
-            app.UseAuthorization();
+					logger.LogInformation("Recreating database.");
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-    }
+					try {
+						patientServiceDbContext.Database.EnsureDeleted();
+						patientServiceDbContext.Database.EnsureCreated();
+					} catch (NpgsqlException e) {
+						logger.LogError(e, "An error occured when testing database connection.");
+						throw e;
+					}
+
+					logger.LogInformation("Database recreated.");
+
+					if (bool.Parse(Configuration["SeedDatabase"])) {
+						logger.LogInformation("Seeding database.");
+
+						try {
+							SeedData.Initialize(serviceProvider);
+						} catch (NpgsqlException e) {
+							logger.LogError(e, "An error occured while seeding database.");
+							throw e;
+						} catch (KeyNotFoundException e) {
+							logger.LogError(e, "An error occured while parsing json: " + e.Message); 
+							throw e;
+						} catch (ArgumentNullException e) {
+							logger.LogError(e, "An error occured while parsing json: " + e.Message); 
+							throw e;
+						} catch (InvalidOperationException e) {
+							logger.LogError(e, "An error occured while parsing json: " + e.Message); 
+							throw e;
+						}
+
+						logger.LogInformation("Database successfully seeded.");
+					}else {
+						logger.LogInformation("Skipping seeding database.");
+					}
+				} else {
+					app.UseExceptionHandler("/Error");
+				}
+			}
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
 }
