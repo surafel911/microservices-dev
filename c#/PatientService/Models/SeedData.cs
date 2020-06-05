3d8af8f0e800119ef459e0a95c42d28662a7415e
Surafel Assefa <surafel911@gmail.com>
2020-06-05 18:47:52 -0400

    Cleans up code.

--- c#/PatientService/Models/SeedData.cs
+++ c#/PatientService/Models/SeedData.cs
@@ -11,19 +11,47 @@ using Microsoft.Extensions.DependencyInjection;
 using PatientService.Data;
 using PatientService.Services;
 
-/*
- * TODO: Use this API to seed users https://randomuser.me/api
- */
 namespace PatientService.Models
 {
     public static class SeedData
     {
-		private static readonly int PatientCount = 5000;
+		// TODO: Have this be replaced by a config value
+		private static readonly int PatientCount = 100;
 
 		private static async Task<string> GetRandomPatientData(HttpClient httpClient)
 		{
             return await httpClient.GetStringAsync(
 				"https://randomuser.me/api/?nat=us&results=" + PatientCount.ToString());
+		}
+
+		private static Patient GetPatientFromJsonElement(JsonElement patientElement)
+		{
+			JsonElement idElement = patientElement.GetProperty("id");
+			JsonElement nameElement = patientElement.GetProperty("name");
+			JsonElement dateOfBirthElement = patientElement.GetProperty("dob");
+				
+			return new Patient 
+				{
+					Id = Guid.NewGuid(),
+					FirstName = nameElement.GetProperty("first").GetString(),
+					LastName = nameElement.GetProperty("last").GetString(),
+					LastFourOfSSN = idElement.GetProperty("value").GetString().Substring(7),
+					DateOfBirth = dateOfBirthElement.GetProperty("date").GetDateTime()
+				};
+		}
+
+		private static PatientContact GetPatientContactFromJsonElement(JsonElement patientElement, Patient patient)
+		{
+			JsonElement emailAddressElement = patientElement.GetProperty("email");
+			JsonElement phoneNumberElement = patientElement.GetProperty("cell");
+	
+			return new PatientContact
+				{
+					PatientId = patient.Id,
+					Patient = patient,
+					PhoneNumber = phoneNumberElement.GetString(),
+					EmailAddress = emailAddressElement.GetString()
+				};
 		}
 
         public static void Initialize(IServiceProvider serviceProvider)
@@ -51,29 +79,10 @@ namespace PatientService.Models
 				.RootElement.GetProperty("results");
 				
 			foreach (JsonElement patientElement in patientsElement.EnumerateArray()) {
- 				JsonElement idElement = patientElement.GetProperty("id");
-				JsonElement nameElement = patientElement.GetProperty("name");
-				JsonElement emailAddressElement = patientElement.GetProperty("email");
-				JsonElement dateOfBirthElement = patientElement.GetProperty("dob");
-				JsonElement phoneNumberElement = patientElement.GetProperty("cell");
-	
-				Patient patient = new Patient 
-					{
-						Id = Guid.NewGuid(),
-						FirstName = nameElement.GetProperty("first").GetString(),
-						LastName = nameElement.GetProperty("last").GetString(),
-						LastFourOfSSN = idElement.GetProperty("value").GetString().Substring(7),
-						DateOfBirth = dateOfBirthElement.GetProperty("date").GetDateTime()
-					};
-	
-				PatientContact patientContact = new PatientContact
-					{
-						PatientId = patient.Id,
-						Patient = patient,
-						PhoneNumber = phoneNumberElement.GetString(),
-						EmailAddress = emailAddressElement.GetString()
-					};
-			
+				Patient patient = GetPatientFromJsonElement(patientElement);
+
+				PatientContact patientContact = GetPatientContactFromJsonElement(patientElement, patient);
+
 				patients.Add(patient);
 				patientContacts.Add(patientContact);
 			}
