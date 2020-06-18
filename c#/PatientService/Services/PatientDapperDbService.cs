ac1f5200b8051ea1c51e2df9eb9602fe40d2942a
Surafel Assefa <surafel911@gmail.com>
2020-06-18 17:43:37 -0400

    Adds not implemented exceptions.

--- c#/PatientService/Services/PatientDapperDbService.cs
+++ c#/PatientService/Services/PatientDapperDbService.cs
@@ -18,68 +18,77 @@ namespace PatientService.Services
 
 		public bool CanConnect()
 		{
-			return false;
+			throw new NotImplementedException();
 		}
 
 		public void EnsureCreated()
 		{
+			throw new NotImplementedException();
 		}
 
 		public void EnsureDeleted()
 		{
+			throw new NotImplementedException();
 		}
 
 		public bool AnyPatients()
 		{
-			return false;
+			throw new NotImplementedException();
 		}
 
 		public void AddPatient(Patient patient)
 		{
+			throw new NotImplementedException();
 		}
 
 		public void AddPatientRange(IEnumerable<Patient> patients)
 		{
+			throw new NotImplementedException();
 		}
 
 		public Patient FindPatient(Guid id)
 		{
-			return null;
+			throw new NotImplementedException();
 		}
 
 		public Patient FindPatient(string firstName, string lastName, DateTime dateOfBirth)
 		{
-			return null;
+			throw new NotImplementedException();
 		}
 
 		public void UpdatePatient(Patient patient)
 		{
+			throw new NotImplementedException();
 		}
 
 		public void RemovePatient(Patient patient)
 		{
+			throw new NotImplementedException();
 		}
 
 		public void AddPatientContact(PatientContact patientContact)
 		{
+			throw new NotImplementedException();
 		}
 
 		public void AddPatientContactRange(IEnumerable<PatientContact> patientContacts)
 		{
-		
+			throw new NotImplementedException();
 		}
 
 		public PatientContact FindPatientContact(Guid patientId)
 		{
-			return null;
+			throw new NotImplementedException();
 		}
 
 		public void RemovePatientContact(PatientContact patientContact)
 		{
+			throw new NotImplementedException();
 		}
 
 		public void UpdatePatientContact(PatientContact patientContact)
 		{
+			throw new NotImplementedException();
 		}
     }
 }
