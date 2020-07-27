126c8c576e3b1cb8310fb00ce2536d5627841642
Surafel Assefa <surafel911@gmail.com>
2020-07-26 20:23:14 -0400

    Implements remaining unit tests.

--- c#/PatientServiceTest/PatientServiceUnitTest.cs
+++ c#/PatientServiceTest/PatientServiceUnitTest.cs
@@ -19,13 +19,14 @@ namespace PatientServiceTest
     public class PatientServiceUnitTest : IDisposable
     {
 		private readonly Patient _patient;
+		private readonly PatientContact _patientContact;
 		private readonly PatientController _patientController;
 		private readonly MockPatientDbService _mockPatientDbService;
 
 		public PatientServiceUnitTest()
 		{
 			// Setup
-			_patient = new Patient
+			_patient = new Patient()
 			{
 				Id = Guid.NewGuid(),
 				FirstName = "John",
@@ -34,8 +35,16 @@ namespace PatientServiceTest
 				DateOfBirth = DateTime.Now.AddDays(-7),
 			};
 
+			_patientContact = new PatientContact()
+			{
+				Patient = _patient,
+				PatientId = _patient.Id,
+				PhoneNumber = "666-666-6666",
+			};
+
 			_mockPatientDbService = new MockPatientDbService();
 			_mockPatientDbService.AddPatient(_patient);
+			_mockPatientDbService.AddPatientContact(_patientContact);
 
 			_patientController = new PatientController(
 				new NullLogger<PatientController>(),
@@ -53,7 +62,7 @@ namespace PatientServiceTest
 		// TODO: Add tests that look into mock db and check if patients are really there.
 
 		[Fact]
-		public void TestGetPatientFromIdValidId()
+		public void TestGetPatientFromId()
 		{
 			// Act
 			IActionResult result = _patientController.GetPatient(_patient.Id);
@@ -72,13 +81,12 @@ namespace PatientServiceTest
 
 			// Assert
 			Assert.IsType<ObjectResult>(result);
-			Assert.NotNull(((ObjectResult)result).Value);
 			Assert.IsType<ProblemDetails>(((ObjectResult)result).Value);
 			Assert.Equal(StatusCodes.Status404NotFound, ((ObjectResult)result).StatusCode);
 		}
 
         [Fact]
-        public void TestGetPatientFromQueryValidQuery()
+        public void TestGetPatientFromQuery()
 		{
 			// Act
 			IActionResult result = _patientController.GetPatient(
@@ -103,10 +111,10 @@ namespace PatientServiceTest
 		}
 
 		[Fact]
-		public void TestCreatePatientValidPatient()
+		public void TestCreatePatient()
 		{
 			// Act
-			IActionResult result = _patientController.CreatePatient(new Patient {
+			IActionResult result = _patientController.CreatePatient(new Patient() {
 				FirstName = "Adam",
 				LastName = "Johnson",
 				LastFourOfSSN = "5678",
@@ -131,19 +139,23 @@ namespace PatientServiceTest
 		}
 
 		[Fact]
-		public void TestUpdatePatientValidPatient()
+		public void TestUpdatePatient()
 		{
 			// Act
 			string firstName = "Jacob", lastName = "Dunham";
-			Patient patient = _patient;
 
-			patient.FirstName = firstName;
-			patient.LastName = lastName;
+			PatientDTO patientDTO = new PatientDTO()
+			{
+				FirstName = firstName,
+				LastName = lastName,
+				DateOfBirth = _patient.DateOfBirth,
+				LastFourOfSSN = _patient.LastFourOfSSN,
+			};
 
-			IActionResult result = _patientController.UpdatePatient(patient.Id, patient);
+			IActionResult result = _patientController.UpdatePatient(_patient.Id, patientDTO);
 
 			// Assert
-			patient = _mockPatientDbService.PatientList.Find(p => p.Id == patient.Id);
+			Patient patient = _mockPatientDbService.PatientList.Find(p => p.Id == _patient.Id);
 
 			Assert.IsType<NoContentResult>(result);
 			Assert.Equal(patient.FirstName, firstName);
@@ -154,24 +166,24 @@ namespace PatientServiceTest
 		public void TestUpdatePatientInvalidPatient()
 		{
 			// Act
-			Patient patient = new Patient();
+			PatientDTO patientDTO = new PatientDTO()
+			{
+				FirstName = "Jacob",
+				LastName = "Dunham",
+				DateOfBirth = _patient.DateOfBirth,
+				LastFourOfSSN = _patient.LastFourOfSSN,
+			};
 
-			patient.Id = new Guid();
-			patient.FirstName = "Jacob";
-			patient.LastName = "Dunham";
-			patient.DateOfBirth = _patient.DateOfBirth;
-			patient.LastFourOfSSN = _patient.LastFourOfSSN;
-
-			IActionResult result = _patientController.UpdatePatient(patient.Id, patient);
+			IActionResult result = _patientController.UpdatePatient(Guid.NewGuid(), patientDTO);
 
 			// Assert
 			Assert.IsType<ObjectResult>(result);
-			Assert.IsType<ProblemDetails>(((ObjectResult)result).Value);
 			Assert.Equal(StatusCodes.Status404NotFound, ((ObjectResult)result).StatusCode);
+			Assert.IsType<ProblemDetails>(((ObjectResult)result).Value);
 		}
 
 		[Fact]
-		public void TestDeletePatientValidPatient()
+		public void TestDeletePatient()
 		{
 			// Act
 			IActionResult result = _patientController.DeletePatient(_patient.Id);
@@ -185,16 +197,256 @@ namespace PatientServiceTest
 		public void TestDeletePatientDoesNotExist()
 		{
 			// Act
-			Patient patient = new Patient();
-
-			patient.Id = new Guid();
-			patient.FirstName = _patient.FirstName;
-			patient.LastName = _patient.LastName;
-			patient.DateOfBirth = _patient.DateOfBirth;
-			patient.LastFourOfSSN = _patient.LastFourOfSSN;
+			Patient patient = new Patient()
+			{
+				Id = Guid.NewGuid(),
+				FirstName = _patient.FirstName,
+				LastName = _patient.LastName,
+				DateOfBirth = _patient.DateOfBirth,
+				LastFourOfSSN = _patient.LastFourOfSSN,
+			};
 
 			IActionResult result = _patientController.DeletePatient(patient.Id);
 
+			// Assert
+			Assert.IsType<ObjectResult>(result);
+			Assert.IsType<ProblemDetails>(((ObjectResult)result).Value);
+			Assert.Equal(StatusCodes.Status404NotFound, ((ObjectResult)result).StatusCode);
+		}
+
+		[Fact]
+		public void TestGetPatientContactValidPatientContact()
+		{
+			// Act
+			IActionResult result = _patientController.GetPatientContact(_patient.Id);
+
+			// Assert
+			Assert.IsType<OkObjectResult>(result);
+			Assert.IsType<PatientContact>(((OkObjectResult)result).Value);
+			Assert.Equal(((OkObjectResult)result).Value, _patientContact);
+		}
+
+		[Fact]
+		public void TestGetPatientContactInvalidPatient()
+		{
+			// Act
+			IActionResult result = _patientController.GetPatientContact(new Guid());
+
+			// Assert
+			Assert.IsType<ObjectResult>(result);
+			Assert.IsType<ProblemDetails>(((ObjectResult)result).Value);
+			Assert.Equal(StatusCodes.Status404NotFound, ((ObjectResult)result).StatusCode);
+		}
+
+		[Fact]
+		public void TestGetPatientContactInvalidPatientContact()
+		{
+			// Act
+			Patient patient = new Patient()
+			{
+				Id = Guid.NewGuid(),
+				FirstName = _patient.FirstName,
+				LastName = _patient.LastName,
+				DateOfBirth = _patient.DateOfBirth,
+				LastFourOfSSN = _patient.LastFourOfSSN,
+			};
+
+			_mockPatientDbService.AddPatient(patient);
+
+			IActionResult result = _patientController.GetPatientContact(patient.Id);
+
+			// Assert
+			Assert.IsType<ObjectResult>(result);
+			Assert.IsType<ProblemDetails>(((ObjectResult)result).Value);
+			Assert.Equal(StatusCodes.Status404NotFound, ((ObjectResult)result).StatusCode);
+		}
+
+		[Fact]
+		public void TestCreatePatientContactValidContact()
+		{
+			// Act
+			Patient patient = new Patient()
+			{
+				Id = Guid.NewGuid(),
+				FirstName = _patient.FirstName,
+				LastName = _patient.LastName,
+				DateOfBirth = _patient.DateOfBirth,
+				LastFourOfSSN = _patient.LastFourOfSSN,
+			};
+
+			PatientContact patientContact = new PatientContact()
+			{
+				Patient = patient,
+				PatientId = patient.Id,
+				PhoneNumber = "666-666-6666",
+			};
+
+			_mockPatientDbService.AddPatient(patient);
+
+			IActionResult result = _patientController.CreatePatientContact(patient.Id, patientContact);
+
+			// Assert
+			Assert.IsType<NoContentResult>(result);
+			Assert.Contains(_mockPatientDbService.PatientContactList, patientContact => patientContact.PatientId == patient.Id);
+		}
+
+		[Fact]
+		public void TestCreatePatientContactInvalidPatient()
+		{
+			// Act
+			Patient patient = new Patient()
+			{
+				Id = Guid.NewGuid(),
+				FirstName = "Jacob",
+				LastName = "Johnson",
+				DateOfBirth = DateTime.Now,
+				LastFourOfSSN = "1234",
+			};
+
+			PatientContact patientContact = new PatientContact()
+			{
+				Patient = patient,
+				PatientId = patient.Id,
+				PhoneNumber = "666-666-6666",
+			};
+
+			IActionResult result = _patientController.CreatePatientContact(patient.Id, patientContact);
+
+			// Assert
+			Assert.IsType<ObjectResult>(result);
+			Assert.IsType<ProblemDetails>(((ObjectResult)result).Value);
+			Assert.Equal(StatusCodes.Status404NotFound, ((ObjectResult)result).StatusCode);
+			Assert.DoesNotContain(_mockPatientDbService.PatientContactList, pc => pc == patientContact);
+		}
+
+		[Fact]
+		public void TestCreatePatientContactAlreadyExists()
+		{
+			// Act
+			PatientContact patientContact = new PatientContact()
+			{
+				Patient = _patient,
+				PatientId = _patient.Id,
+				PhoneNumber = "666-666-6666",
+			};
+
+			IActionResult result = _patientController.CreatePatientContact(_patient.Id, patientContact);
+
+			// Assert
+			Assert.IsType<ObjectResult>(result);
+			Assert.IsType<ProblemDetails>(((ObjectResult)result).Value);
+			Assert.Equal(StatusCodes.Status400BadRequest, ((ObjectResult)result).StatusCode);
+		}
+
+		[Fact]
+		public void TestUpdatePatientContactValidContact()
+		{
+			// Act
+			string phoneNumber = "111-111-1111";
+
+			PatientContactDTO patientContactDTO = new PatientContactDTO()
+			{
+				PhoneNumber = phoneNumber,
+			};
+
+			IActionResult result = _patientController.UpdatePatientContact(_patient.Id, patientContactDTO);
+
+			// Assert
+			PatientContact patientContact = _mockPatientDbService.PatientContactList.Find(p => p.PatientId == _patient.Id);
+
+			Assert.IsType<NoContentResult>(result);
+			Assert.Equal(_patientContact, patientContact);
+		}
+
+		[Fact]
+		public void TestUpdatePatientContactInvalidPatient()
+		{
+			// Act
+			PatientContactDTO patientContactDTO = new PatientContactDTO()
+			{
+				PhoneNumber = "111-111-1111",
+			};
+
+			IActionResult result = _patientController.UpdatePatientContact(Guid.NewGuid(), patientContactDTO);
+
+			// Assert
+			Assert.IsType<ObjectResult>(result);
+			Assert.IsType<ProblemDetails>(((ObjectResult)result).Value);
+			Assert.Equal(StatusCodes.Status404NotFound, ((ObjectResult)result).StatusCode);
+			Assert.NotEqual(_patientContact.PhoneNumber, patientContactDTO.PhoneNumber);
+		}
+
+		[Fact]
+		public void TestUpdatePatientContactInvalidContact()
+		{
+			// Act
+			Patient patient = new Patient()
+			{
+				Id = Guid.NewGuid(),
+				FirstName = "Jack",
+				LastName = "Johnson",
+				DateOfBirth = DateTime.Now,
+				LastFourOfSSN = "1234",
+			};
+
+			PatientContactDTO patientContactDTO = new PatientContactDTO()
+			{
+				PhoneNumber = "111-111-1111",
+			};
+
+			_mockPatientDbService.AddPatient(patient);
+
+			IActionResult result = _patientController.UpdatePatientContact(patient.Id, patientContactDTO);
+
+			// Assert
+			PatientContact patientContact = _mockPatientDbService.PatientContactList.Find(p => p.PatientId == patient.Id);
+
+			Assert.IsType<ObjectResult>(result);
+			Assert.IsType<ProblemDetails>(((ObjectResult)result).Value);
+			Assert.Equal(StatusCodes.Status404NotFound, ((ObjectResult)result).StatusCode);
+			Assert.NotEqual(_patientContact, patientContact);
+		}
+
+		[Fact]
+		public void TestDeletePatientContactValidContact()
+		{
+			// Act
+			IActionResult result = _patientController.DeletePatientContact(_patient.Id);
+			
+			// Assert
+			Assert.IsType<NoContentResult>(result);
+			Assert.DoesNotContain(_mockPatientDbService.PatientContactList, patientContact => patientContact == _patientContact);
+		}
+
+		[Fact]
+		public void TestDeletePatientContactInvalidPatient()
+		{
+			// Act
+			IActionResult result = _patientController.DeletePatientContact(new Guid());
+			
+			// Assert
+			Assert.IsType<ObjectResult>(result);
+			Assert.IsType<ProblemDetails>(((ObjectResult)result).Value);
+			Assert.Equal(StatusCodes.Status404NotFound, ((ObjectResult)result).StatusCode);
+		}
+		
+		[Fact]
+		public void TestDeletePatientContactInvalidContact()
+		{
+			// Act
+			Patient patient = new Patient()
+			{
+				Id = Guid.NewGuid(),
+				FirstName = "Jack",
+				LastName = "Johnson",
+				DateOfBirth = DateTime.Now,
+				LastFourOfSSN = "1234",
+			};
+
+			_mockPatientDbService.AddPatient(patient);
+			
+			IActionResult result = _patientController.DeletePatientContact(patient.Id);
+			
 			// Assert
 			Assert.IsType<ObjectResult>(result);
 			Assert.IsType<ProblemDetails>(((ObjectResult)result).Value);
